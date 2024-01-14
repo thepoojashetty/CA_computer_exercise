feb30036d9da0cbe08a5e535ff70917c7ecfacf0
rakesh-1597 <bhatrakesh930@gmail.com>
2024-01-14 12:20:18 +0100

    modified tinyx,use salloc first to get a gpu core first, then run this sh file

--- ex_08/scripts/tinyx.sh
+++ ex_08/scripts/tinyx.sh
@@ -1,24 +1,25 @@
 #!/bin/bash -l
 #SBATCH --job-name=CA_EX8_stream
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=1
-#SBATCH –gres=gpu:rtx3080:1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:rtx3080:1
 #SBATCH --exclusive
 #SBATCH -t 02:00:00
 #SBATCH -o /home/hpc/rzku/hpcv651h/ex_08/ex08_stream.out
 #SBATCH -e /home/hpc/rzku/hpcv651h/ex_08/ex08_stream.err
+
 
 # Enable debug and verbose mode
 set -x
 set -v
 
 # Load module with icc compiler
-module load intel
-module load cuda
+# module load intel
+# module load cuda
 
-# TODO allocate a compute node
 
-srun likwid-pin -c N:0-7
+#salloc.tinygpu --gres=gpu:rtx3080:1 --time=00:30:00
+srun ../bin/stream
 
 touch ready
 
